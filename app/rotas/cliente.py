c07b2c5276a4d3d8dfab47fb61c3415103977220
Jerônimo <karkwogvoldor@gmail.com>
2026-08-12 18:30:28 -0300

    feat: adiciona banco SQLite assíncrono com gerenciador de contexto e Depends no FastAPI

--- app/rotas/cliente.py
+++ app/rotas/cliente.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.modelos.cliente import Cliente
+from app.banco_de_dados.cliente_repositorio import ClienteRepositorio
+from app.dependencias import obter_cliente_repositorio
 
 router = APIRouter(
     prefix="/clientes"
@@ -14,15 +18,17 @@ CLIENTE_LIST = [
         ]
 
 @router.get("/", response_model=list[Cliente])
-async def listar_clientes():
-    return CLIENTE_LIST
+async def listar_clientes(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)]):
+    return await cliente_repositorio.listar_clientes()
 
 @router.get("/{cliente_id}", response_model = Cliente | None)
-async def obter_cliente(cliente_id: int):
-    for cliente in CLIENTE_LIST:
-        if cliente.id_ == cliente_id:
-            return cliente
-    
+async def obter_cliente(
+    cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)],
+    cliente_id: int
+):
+    cliente = await cliente_repositorio.obter_cliente(cliente_id)
+
+    if not cliente:
+        raise HTTPException(status_code=404, detail="Cliente não encontrado!")
+
     return None
-
-
