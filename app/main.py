7eace3beed8454b0c07c7f3a8818e9f70a8b167e
Jerônimo <karkwogvoldor@gmail.com>
2026-08-18 08:36:32 -0300

    configurando pagina de login

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from app.rotas import cliente
+from app.rotas import cliente, login
 
 templates = Jinja2Templates(directory="templates")
 
@@ -17,6 +17,7 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(cliente.router)
 app.include_router(cliente.front_router)
+app.include_router(login.router)
 
 
 
